da04419c195ad40f24f5311109f2f7eb93f6dc79
John Peterson <johnp5419@gmail.com>
2025-03-31 23:55:24 -0600

    Initialize Repository
    The repository has been created and submodule dependencies for glfw and imgui have been added. The main.cpp file and now modified CMakeLists.txt file come from the imgui example_glfw_vulkan.

---
+++ build/CMakeFiles/imgui-example.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui-example.dir/deps/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/backends/imgui_impl_vulkan.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/backends/imgui_impl_vulkan.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/imgui-example.dir/deps/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/imgui-example.dir/src/main.cpp.o"
+  "CMakeFiles/imgui-example.dir/src/main.cpp.o.d"
+  "imgui-example"
+  "imgui-example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui-example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/imgui-vulkan.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/backends/imgui_impl_vulkan.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/backends/imgui_impl_vulkan.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/deps/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/imgui-vulkan.dir/src/main.cpp.o"
+  "CMakeFiles/imgui-vulkan.dir/src/main.cpp.o.d"
+  "imgui-vulkan"
+  "imgui-vulkan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui-vulkan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
